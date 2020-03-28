731ab532702f3822269922c5eaf4f200c0a21b0c
Spencer Bangterer <bangerter.spencer@gmail.com>
2020-03-28 13:06:23 -0600

    Set up stats page routing

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -8,4 +8,9 @@ router.get("/", function(req, res) {
     res.sendFile(path.join(__dirname, "../public/exercise.html"));
   });
 
+  router.get("/stats", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/stats.html"));
+  });
+
+
 module.exports = router;
